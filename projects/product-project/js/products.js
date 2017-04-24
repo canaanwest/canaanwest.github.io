ab6a1072501e63f6c4398c6df316eb783485ca8f
Canaan West <canaan.a.west@gmail.com>
2017-04-24 21:30:01 +0000

    * product project

--- projects/product-project/js/products.js
+++ projects/product-project/js/products.js
@@ -1,108 +1,329 @@
 /* globals $ _ */
 
-//asyncronous because we don't know how long it will take. so we use the A.F., called a callback function or callback pattern, meaning it does the event when the event that is being listened for is heard.
-$(document).on("ready", onDocReady); 
+//call-back pattern - event driven architech
+$(document).ready(function() {
+    $.getJSON('data/product.json', onProductData);
+});
 
-function onDocReady(){ //this tells js to wait for a ready event. waits for the DOM to be fully ready; otherwise, it will execute before the doc is ready. 
-    console.log("DOM is ready!");
-$.getJSON("data/product.json", onProductData)
-.fail(function(){console.log("getJSON on product.json failed")})
-    
+function onProductData(products) {
+    //initilaize ui 
+    initilaizeUI(products);
+    showProducts(products);
+    findMyTerm(products);
+    filterAutoPopulate(whatInDatFilter(products))
+    filterBy(products)
+    ifAnythingClicked(products);
+    lightBox(products)
+    pricesArray(products);
+    highToLow(products); 
+    hideDirections(products); 
 }
 
-function onProductData(products){
-    console.log(products);
-    //initalize ui
-    initalizeUI(products); 
-    //show all products
-    showProducts(products); 
-}    
-
-function initalizeUI(products){
-    //create static product UL
-    $("<ul>")
-        .attr("id", "products")
-        .addClass("list-products")
-        .appendTo("main");
+//create product ul 
+function initilaizeUI(products) {
+    $('<ul>')
+        .attr('id', 'products')
+        .addClass('list-products')
+        .appendTo('main');
 }
-    
-    
-function showProducts(products){
-    //needs to work at the outset, when the user does a keyword search, or when the user uses the filter
-    //clear the DOM
-    $("#products")
-        .empty() //this empties any of the products that are in the DOM.
+
+//show all products showProducts(products) 
+
+function showProducts(products) {
+    //clear any products in dom - using empty function 
+    $('#products')
+        .empty()
         .append(createProductListItems(products));
-        //add event listeners to handle the clicks
+        lightBox(products)
+        findMyTerm(products)
+        //add event listerners 
 }
 
-function createProductListItems(products){
-    //map has to return some transformation of the products array
-    return _.map(products, function(product){
-        return $("<li>")
-            .addClass("li-product")
-            .data("product", product)
-            //wrap your image in a div
-            .text(product.desc)
-            .append(createProductImageDiv(`img/product/thumbs/${product.img}`, "product-thumb"))
-            .append(createProductDetailsDiv(product.desc, product.price, product.stock))
-    })
-}
+//create all list items 
 
-function createProductImageDiv(url, cssClass){
-  //use jQ to create and return div that wraps an image that uses the url as its src.
-  return $("div")
-};
-
-function createProductDetailsDiv(desc, price, stock){
-    //create div with 3 child divs, one for each param, give each a unique class.
-}
-
-function onProductClicked(event){
-    const product = $(event.currentTarget).data("product"); 
-    showProductDetails(product);
-}
-
-
-function showProductDetails(product){
-    //create markup (use $ to create the html to render the product details) for product detail
-    //show it in a pop up
-}
-
-
-//SEARCH FUNCTION
-
-function search(collection, term){
-    //1. create something to collect your output
-    //2. iterate through your collection //each, reduce, could probably do it with filter, and you HAVE TO HIT every object
-    
-
-    _.reduct(collection, function(values){
-         //recursive case://
-    //1. is this value a collection?
-    //2. if yes call search(value, term)
-    //3. we could check the length of the inner "output array"; if the recursive case comes back with any length, we know we want to push the product to the outer array 
-    
-    //base case: //
-    //1. is it a string? 2. does it contain the search term? 3. if yes, collect the product into the output; else 
-    //search should be case insensitive
+function createProductListItems(products) {
+    return _.map(products, function(product) {
+         return  $('<li>') //outer box
+            .addClass('li-product')
+            .data('product', product)
+            //desc, price, stock, image(wrap in a div) factories 'FLEXBOX!'
+            //.text(product.desc)
+            .append(createProductImageDiv(`img/product/thumbs/${product.image}`, 'product-thumb'))
+            .append(createProductsDetailsDiv(product.desc, product.price, product.stock))
+            .append(createProductImageDiv(`img/product/${product.image}`, 'product-image').hide())
+            .attr("image", product.image)
+            .attr("desc", product.desc)
+            .attr("type", product.type)
+            .attr("availableColors", product.availableColors)
+            .attr("specs", product.specs)
+            .attr("price", product.price)
+            .attr("stock", product.stock);
         
+    }); 
+}
+
+ 
+function createProductImageDiv(url, cssClass) {
+  //use jquery to create and return a div that wraps an image that uses the url as its src   
+    //cssClass makes more flexible 
+        return $('<div>')
+            .append("<img src='" + url + "'>")
+            .addClass(cssClass)
+            
+          //  .data('product', url)
+       // $(".li-product").prepend( '<img class="thumb" src="img/product/thumbs/edge-gold.jpg">');
+ }
+
+function createProductsDetailsDiv(desc, price, stock){
+    return $("<div>")
+            .addClass("product-details")
+            .append($("<div>").addClass("product-description").text(desc))
+            .append($("<div>").addClass("product-price").text("Yours for $" + price + "!!"))
+            .append($("<div>").addClass("product-stock").text("Items Remaining: " + stock));
+    
+    
+    //create div with 3 children divs, one for desc, one for price, one for stock 
+    // give each a unique class
+}
+
+
+//THIS WORKS
+function findMyTerm(products){
+$("#search-me-pls").on("click", function(){
+    let term = $("input.form-control").val();
+    showProducts(search(products, term));
+    lightBox(products); 
+});
+}
+
+//RECURSIVE SEARCH FUNCTION HAS A BUG
+function search(products, term){
+    // 1. create output array to collect your output []
+    // 2. iterate collection .each or .reduce(hipper)   
+    return _.reduce(products, function(previousProducts, currentProduct, i, product){
+          if(isCollection(currentProduct)){
+            if (search(currentProduct, term).length){
+                previousProducts.push(currentProduct); 
+            }
+        } 
+        
+        else if (typeof currentProduct === "string"){
+            if(currentProduct.toLowerCase().search(term.toLowerCase()) > -1){
+                 previousProducts.push(currentProduct); 
+            }
+        }  
+        return previousProducts;
+        
+    }, []);
+
+}
+
+// iscollection, scratchpad - is array or object? 
+    //is this value a collection?
+    //IF yes, call search(value, term)
+    
+    // base case: IF is this a string?(*subsearch* / substring) & IF this string contain search term? (must be case insensitve)
+                //3.  IF yes, collect this product into your output array 
+    //recurisve function 
+    //has own array??? "match" value is pushed into array, resolves to result of calling search IF subsearch match 
+    //IF search.length 0 = falsy 
+    //value = product  (can happen many times because nested structure)
+     //return the output thingy (collection) array 
+
+
+function isCollection(value) {
+    if(value === "null"){
+        return false;
+    } else if (value instanceof Date){
+        return false;
+    } else if (Array.isArray(value)){
+        return true;
+    } else if (typeof value === "object"){
+        return true;
+    } else if (typeof value !== "object"){
+      return false;
+    }
+    //is this value an array or true object 
+      // iscollection, scratchpad - is array or object? 
+}
+
+
+
+
+
+
+//FILTER FUNCTIONS
+//identify the product type, puts the type into an object if it's not there; pushes the object for the item to an array in the approprate "type" key
+function whatInDatFilter(products){
+    var container = {};
+    _.reduce(products, function(filteredProds, currentProd, i, products){
+        if(!container.hasOwnProperty(currentProd["type"])){
+            container[currentProd["type"]] = [currentProd]; 
+        } else if (container.hasOwnProperty(currentProd["type"])) {
+             container[currentProd["type"]].push(currentProd);
+    }}, {});
+    
+    return container;
+    
+
+}
+
+ 
+ // THIS WORKS! //iterate through the output of whatInDatFilter to autopopulate dropdown w/info
+function filterAutoPopulate(products){
+    var theFilter = _.each(products, function(val, key, products){
+        $("<li>").addClass("filtered-products").attr("id", key).text(key + "s " + " (" + val.length + ") ").prependTo(".dropdown-menu").append($("<li>").addClass("divider").appendTo(".dropdown-menu"));
+})
+        $("<li>").addClass("prices-low-to-high").attr("id", "prices-low-to-high").text("Prices: Low to High").prependTo(".dropdown-menu").append($("<li>").addClass("divider").appendTo(".dropdown-menu"));
+        $("<li>").addClass("prices-high-to-low").attr("id", "prices-high-to-low").text("Prices: High to Low").prependTo(".dropdown-menu").append($("<li>").addClass("divider").appendTo(".dropdown-menu"));; 
+        $("<li>").addClass("anything").attr("id", "any-product").text("All Products").appendTo(".dropdown-menu");
+ 
+}
+
+
+
+function filterBy(products){
+    $(".filtered-products").click(function(){
+        let productType = $(event.currentTarget); //this allows the program to focus in and identify different attr of the clicked item
+        showProducts(whatInDatFilter(products)[productType.attr("id")]); //makes use of the event.currentTarget to call the ID of the current target as the key of the output object of the functioncall
+        
+    });
+    
+    /////////////////////////
+    //PERHAPS we want this filter to narrow in on another filter we've already selected?????
+     $(".prices-low-to-high").click(function(){
+        let productType = $(event.currentTarget); //this allows the program to focus in and identify different attr of the clicked item
+        showProducts(lowToHigh(products)); //makes use of the event.currentTarget to call the ID of the current target as the key of the output object of the functioncall
+    });
+
+     $(".prices-high-to-low").click(function(){
+        let productType = $(event.currentTarget); //this allows the program to focus in and identify different attr of the clicked item
+        showProducts(highToLow(products)); //makes use of the event.currentTarget to call the ID of the current target as the key of the output object of the functioncall
+         
+     })
+        
+        
+    };
+    
+
+function ifAnythingClicked(products){
+    $(".anything").click(function(){
+        return showProducts(products); 
     })
-    
-  //return the output collection (which schould be an array)
-    
+
 }
 
 
 
-
-function isCollection(value){
-    //is it a collection??
+function pricesArray(products){
+  let output = []
+  _.each(products, function(val, i, products){
+       output.push(val.price)})
+    output.sort(function(a, b){
+        return a-b;
+    });
+   return _.unique(output); 
 }
 
+function lowToHigh(products){
+    let orderedArray = [];
+    _.each(pricesArray(products), function(val, pos, prices){
+        _.each(products, function(item, i, products){
+            //console.log("prices", val); 
+            if(item.price === prices[pos]){
+                orderedArray.push(item); 
+        }
+        })
+    })
+    return orderedArray; 
+}
 
-//remember that functions assigned to constants like this are only stored in memory at the time they are written.
-/*const onDocReady = function(){
-    $(document).off("ready", onDocReady);   // this allows us to remove an event listener by passing it an event and the HANDLER (the function, which must be named)
-    console.log("DOM is ready");        //after this point, it's no longer listening for the event. 
-}*/
+//on click function to call this and pass it to showProducts()
+function highToLow(products){
+    let orderedArray = [];
+    _.each(pricesArray(products).reverse(), function(val, pos, prices){
+        _.each(products, function(item, i, products){
+            //console.log("prices", val); 
+            if(item.price === prices[pos]){
+                orderedArray.push(item); 
+        }
+        })
+    })
+    return orderedArray; 
+}
+
+    
+    
+//NOTES for FILTERBY
+    //needs a click event handler
+    //needs to call the showProducts function for those products with the "type" key matching the id of the clicked;
+    //the relevant items are already in an array inside of the object created by the whatInDatFilt er
+    /*_.each(products, function(val, i, products){
+    if (products["type"] === event.currentTarget.attr("id")){
+        relevantProducts.push(val);*/
+    //identify the product type
+    //select by product type when clicked. $(filtertab).on("click", function that pulls up the items that match the type)
+
+
+//LIGHTBOX
+
+
+//lightbox
+//lightbox
+//lightbox
+
+function lightBox(products) {
+    $('.li-product').on("click", function(event) {
+        let product = $(event.currentTarget) // pulling data from object clicked
+        let src = product.attr("image"); 
+       // let details = showProductDetails(product); 
+       // console.log(details); 
+    /*    $(".recordings-title").click(function() {
+        let recordingArt = $(event.currentTarget);
+        console.log(recordingArt);
+        $(".image-recordings").attr('src', recordingArt.attr('art'));
+    });
+        */
+        
+    
+    if (!$("#light-box").length > 0) {
+      $("body").append("<div id='light-box'>") //<img src='img/product/"  + src + "'><span class='material-icons'>CLOSE</span></div>")
+      //$("<img class='light-box-image' src='img/product/"  + src + "'>")//.appendTo("#light-box")
+      $("<span class='material-icons'>CLOSE</span>").appendTo("#light-box")
+      $("<div>").addClass("product-detail-text").appendTo("#light-box")
+        .append($("<div id='light-box-image'><img class='light-box-image' src='img/product/"  + src + "'>"))
+           // .append("<li>").text(product.attr("image"))
+        .append($("<div id='product-detail-text'>"), 
+                $("<li>").attr("id", "detail-text").text("This " + product.attr("type").toUpperCase() + " can be yours for $" + product.attr("price") + "!"),
+                $("<li>").attr("id", "detail-text").text(product.attr("desc")),
+                $("<li>").attr("id", "detail-text").text("Available in " + product.attr("availableColors")),
+                $("<li>").attr("id", "detail-text").text("CHECK THE SPECS: " + product.attr("specs")),
+                $("<li>").attr("id", "detail-text").text("Number Remaining: " + product.attr("stock")));
+     // $("#light-box").append("<li>").data("data", src);
+      $("#light-box").show();
+      $(".search-bar").hide();
+     
+      
+       $("body").on("click", "#light-box span", function() {
+    $("#light-box").remove();
+     $(".search-bar").show();
+       
+   })
+    } else {
+        $("#light-box").show();
+        $(".search-bar").hide();
+        $("body")
+      //.append("<div id='light-box'><img class='light-box-image' src='img/product/"  + src + "'><span class='material-icons'>CLOSE</span></div>");
+        $("body").on("click", "#light-box span", function() {
+            $("#light-box").remove()
+                $(".search-bar").show();
+
+   })
+    }
+  });
+ 
+  }
+  
+function hideDirections(products){
+    $("#btnDirections").hide(); 
+}
